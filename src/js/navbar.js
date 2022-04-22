a777bca691e110eeb739ba595f8ce838e80e42fa
Sushanghimire-151 <sushanghimire151@gmail.com>
2022-04-22 16:08:47 +0545

    PS and ABM page responsive

---
+++ src/js/navbar.js
@@ -0,0 +1,15 @@
+let activeClass = document.getElementById("active").innerHTML;
+document.getElementById("active-class").innerHTML = activeClass;
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
